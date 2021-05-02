a993e81fc01d3548c2a09d5d079ef604399398ef
Adewale Orotayo <didymus7007@gmail.com>
2021-05-02 23:48:47 +0100

    Installed redux dev tool Moved App.js to components folder Corrected path errors in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import booksReducer from './redux/reducers/books';
-import App from './App';
+import App from './component/App';
 
 const store = createStore(booksReducer);
 
