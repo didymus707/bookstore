9ec4d57f38b70021dd8291922c7403b63a716e92
Adewale Orotayo <didymus7007@gmail.com>
2021-05-06 22:24:06 +0100

    Added header to the app and prepping for styles

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,12 @@
 import BooksList from '../containers/BooksLists';
 import BooksForm from '../containers/BooksForm';
+import Header from './Header';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BooksList />
       <BooksForm />
     </div>
---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <nav>
+    <ul>
+      <li>Bookstore CMS</li>
+      <li>Books</li>
+      <li>Categories</li>
+    </ul>
+    <div className="icon">Person icon</div>
+  </nav>
+);
+
+export default Header;
