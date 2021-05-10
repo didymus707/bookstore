ab86057e71ea3e0e59941e1ee635483c6137f185
Adewale Orotayo <didymus7007@gmail.com>
2021-05-10 22:13:09 +0100

    Added person icon

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { GoPerson } from 'react-icons/go';
+// import { IconContext } from 'react-icons/lib';
+
+const styles = {
+  border: '1px solid #e8e8e8',
+  borderRadius: '50%',
+  color: '#0290ff',
+  padding: '14px',
+};
 
 const Header = () => (
   <nav className="nav">
@@ -7,7 +16,9 @@ const Header = () => (
       <li>BOOKS</li>
       <li>CATEGORIES</li>
     </ul>
-    <div className="icon">Person icon</div>
+    <div className="icon">
+      <GoPerson style={styles} />
+    </div>
   </nav>
 );
 
